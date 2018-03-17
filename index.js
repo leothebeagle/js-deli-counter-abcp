edea7b2bd1039420c2e1f662d9dc63a77652c88a
Aziz Kosber <a.kosber@gmail.com>
2018-03-17 23:21:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing() {
+if (katzDeliLine.length == 0){
+  return "There is nobody waiting to be served!";
+} else {
+  return `Currently serving ${katzDeliLine[0]}`;
+}
+katzDeliLine.shift();
+}
