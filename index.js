1164c898a617d7193e328208173ab45fb045cfdc
Aziz Kosber <a.kosber@gmail.com>
2018-03-18 23:54:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function takeANumber(customers, name) {
   customers.push(name);
   return `Welcome, ${name}. You are number ${customers.length} in line.`
 }
+
+function nowServing() {
+  if (customers.length == 0){
+    return "There is nobody waiting to be served!";
+  } else {
+    return `Currently serving ${customers.shift()}.`
+  }
+}
