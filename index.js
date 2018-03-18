05ab681d860ef0bcfca037fec9d618b06437efa3
Aziz Kosber <a.kosber@gmail.com>
2018-03-18 03:32:21 +0000

    Done.

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
 if (katzDeliLine.length == 0){
   return "There is nobody waiting to be served!";
 } else {
-  return `Currently serving ${katzDeliLine[0]}`;
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
-katzDeliLine.shift();
+}
+
+function currentLine(katzDeliLine) {
+if (katzDeliLine.length == 0){
+  return "The line is currently empty.";
+} else {
+  let line = []
+  let x = katzDeliLine.length
+  for (let i = 0; i < x; i++) {
+    line.push(`${i+1}. ${katzDeliLine[i]}`)
+  }
+  return `The line is currently: ${line.join(', ')}`
+}
 }
