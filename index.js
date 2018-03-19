1ff64966a1092b1ec0552cb2c47d41e045526745
Aziz Kosber <a.kosber@gmail.com>
2018-03-19 00:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ function nowServing(customers) {
     return `Currently serving ${customers.shift()}.`
   }
 }
+
+function currentLine(customers) {
+  if(customers == 0) {
+    return "The line is currently empty."
+  } 
+  let numbersAndNames = []
+    for(let i = 0, x = customers.length; i < x; i++) {
+      numbersAndNames.push(`${i + 1}. ${customers[i]}`)
+    }
+    return `The line is currently: ${numbersAndNames.join(',')}`
+  }
+}
